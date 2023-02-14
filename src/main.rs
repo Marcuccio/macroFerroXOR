d07ed30d7cfc21e91f2b20d541177c5e1068b40d
Marco <marcuccio.dev@gmail.com>
2023-02-14 17:33:01 +0100

    serde v1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   /// Key for XOR
+   #[arg(short, long)]
+   key: String,
+
+   /// URL to call
+   #[arg(short, long)]
+   lhost: String,
+
+   /// URL to call
+   #[arg(short, long, default_value_t="8002")]
+   lport: String,
+
+   /// Number of times to greet
+   #[arg(short, long, default_value_t="C:\\Users\\Public\\")]
+   rhost_local_path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+   let args = Args::parse();
+
+
+
+   for _ in 0..args.count {
+       println!("Hello {}!", args.name)
+   }
 }
